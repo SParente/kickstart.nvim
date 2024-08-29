58f74e0db7c4ebf94f22ea04a055df54caa63124
SParente <sfaparente@gmail.com>
2024-08-29 22:21:51 +0100

    Update fedora.sh

--- os/fedora.sh
+++ os/fedora.sh
@@ -56,6 +56,14 @@ sudo dnf install golang -y
 echo "set -g base-index 1" > ~/.tmux.conf
 
 
-# configs
+# Configs
 export PATH=$PATH:~/go/bin
 export PATH=$PATH:/usr/local/go/bin
+
+# Nerd Fonts
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
+&& cd ~/.local/share/fonts \
+&& unzip JetBrainsMono.zip \
+&& rm JetBrainsMono.zip \
+&& fc-cache -fv
+
