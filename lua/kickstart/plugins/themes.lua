859053764b77c1907fb4dd173b31ba91fca02d61
sergio <sergio@sergio.home>
2024-09-08 02:05:03 +0100

    Add colorscheme and themery to live preview

---
+++ lua/kickstart/plugins/themes.lua
@@ -0,0 +1,8 @@
+return {
+  'sainnhe/everforest',
+  'shaunsingh/nord.nvim',
+  'rose-pine/neovim',
+  'sainnhe/gruvbox-material',
+  'olimorris/onedarkpro.nvim',
+  'loctvl842/monokai-pro.nvim',
+}
